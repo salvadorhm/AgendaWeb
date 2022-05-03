e13731255c671dc23d3d57fb782ba7c1327c30c1
Salvador HM <salvadorhm@gmail.com>
2022-05-03 18:36:02 +0000

    Base de datos creada exitosamente

---
+++ agenda.sql
@@ -0,0 +1,16 @@
+.headers on
+.mode column
+PRAGMA foreign_keys = ON;
+
+CREATE TABLE clientes (
+    id_cliente integer PRIMARY KEY AUTOINCREMENT,
+    nombre varchar(50),
+    email varchar(50)
+);
+
+INSERT INTO clientes(nombre,email)
+VALUES
+('Dejah','dejah@email.com'),
+('Jonh','jonh@email.com');
+
+SELECT * FROM clientes;
